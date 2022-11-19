024515d3c618d3cb01f66842102ae391b956755d
Thinh Truong <Thinhtruong2001@yahoo.com>
2022-11-20 00:10:59 +0700

    update run.bat
    run one for all

---
+++ dev/run.bat
@@ -0,0 +1,7 @@
+@echo off
+set /p venv=< python-venv-name.txt
+
+start title Task-service ^& cd backend\task-service ^& workon %venv% ^& pip install -r requirements.txt ^& python index.py
+start title User-service ^& cd backend\user-service ^& npm i ^& node ./src/index.js
+start title Department-service ^& cd backend\department-service ^& npm i ^& node ./src/index.js
+start title Absence-service ^& cd backend\absence-service ^& npm i ^& node ./src/index.js
